56f7faf4d7b5029910966c07ceacc1510e9fb9ad
DanielRichardson1 <drich1475@gmail.com>
2024-02-15 18:34:18 -0500

    added 2 getter functions

--- Track_Model/Track_Model.py
+++ Track_Model/Track_Model.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 class TrackModel:
     def __init__(self, file_name):
-        d = pd.read_excel(file_name)
+        d = pd.read_excel(file_name, header=None)  # We will also load our header row, and block IDs will map to index
         self.data = d.to_numpy()
         self.line_name = self.data[0, 0]
         self.num_blocks = len(self.data)
@@ -16,4 +16,10 @@ class TrackModel:
 
     def get_num_blocks(self):
         return self.num_blocks
+
+    def set_block_occupancy(self, block_id, occupancy):
+        self.data[block_id, 10] = occupancy
+
+    def get_section(self, section_id):
+        pass  # want this to be fast
 
--- Track_Model/main.py
+++ Track_Model/main.py
@@ -3,10 +3,6 @@ import numpy as np
 
 t = TrackModel('Blue Line.xlsx')
 print(t.get_data())
-data = t.get_data()
-el = data[0, 6]
-print(el)
-print(np.isnan(el))
+t.set_block_occupancy(5, 1)
+print(t.get_data())
 
-print(t.get_line_name())
-print(t.get_num_blocks())
