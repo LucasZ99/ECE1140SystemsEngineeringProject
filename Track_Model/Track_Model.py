d234fd428f92435942d1364f6fc971eb57bead53
DanielRichardson1 <drich1475@gmail.com>
2024-02-15 21:30:32 -0500

    added back-end functions

--- Track_Model/Track_Model.py
+++ Track_Model/Track_Model.py
@@ -7,6 +7,7 @@ class TrackModel:
         self.data = d.to_numpy()
         self.line_name = self.data[0, 0]
         self.num_blocks = len(self.data)
+        self.ticket_sales = 0
 
     def get_data(self):
         return self.data
@@ -17,9 +18,14 @@ class TrackModel:
     def get_num_blocks(self):
         return self.num_blocks
 
-    def set_block_occupancy(self, block_id, occupancy):
-        self.data[block_id, 10] = occupancy
+    def get_ticket_sales(self):
+        return self.ticket_sales
 
     def get_section(self, section_id):
-        pass  # want this to be fast
+        first_index = self.data[:, 1].tolist().index(section_id)
+        last_index = self.num_blocks - self.data[:, 1].tolist()[::-1].index(section_id)
+        return self.data[first_index:last_index, :]
+
+    def set_block_occupancy(self, block_id, occupancy):
+        self.data[block_id, 10] = occupancy
 
--- Track_Model/main.py
+++ Track_Model/main.py
@@ -1,8 +1,5 @@
 from Track_Model import TrackModel
-import numpy as np
 
 t = TrackModel('Blue Line.xlsx')
-print(t.get_data())
-t.set_block_occupancy(5, 1)
-print(t.get_data())
+
 
