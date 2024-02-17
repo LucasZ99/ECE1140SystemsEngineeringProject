7c1a5e5b33951ab99d187ba1aa7e8d731b977f05
DanielRichardson1 <drich1475@gmail.com>
2024-02-16 21:15:18 -0500

    UI grid finished, fill with widgets next

--- Track_Model/Track_Model.py
+++ Track_Model/Track_Model.py
@@ -21,9 +21,11 @@ class TrackModel:
     def get_ticket_sales(self):
         return self.ticket_sales
 
-    def get_section(self, section_id):
-        first_index = self.data[:, 1].tolist().index(section_id)
-        last_index = self.num_blocks - self.data[:, 1].tolist()[::-1].index(section_id)
+    def get_section(self, section_id_start, section_id_end=None):
+        if section_id_end is None:
+            section_id_end = section_id_start
+        first_index = self.data[:, 1].tolist().index(section_id_start)
+        last_index = self.num_blocks - self.data[:, 1].tolist()[::-1].index(section_id_end)
         return self.data[first_index:last_index, :]
 
     def set_block_occupancy(self, block_id, occupancy):
--- Track_Model/main.py
+++ Track_Model/main.py
@@ -1,5 +1,8 @@
 from Track_Model import TrackModel
 
 t = TrackModel('Blue Line.xlsx')
-
-
+print(t.get_data())
+print()
+print(t.get_section('A'))
+print()
+print(t.get_section('B', 'C'))
