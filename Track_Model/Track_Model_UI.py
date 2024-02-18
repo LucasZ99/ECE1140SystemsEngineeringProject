ddf09631be0497b46ec51c4c48b118f8c170f476
DanielRichardson1 <drich1475@gmail.com>
2024-02-18 16:52:25 -0500

    Some UI basics, need to add backend  track model object parameter to track_model_ui window class

--- Track_Model/Track_Model_UI.py
+++ Track_Model/Track_Model_UI.py
@@ -1,9 +1,10 @@
 from PyQt6.QtWidgets import (
     QApplication, QWidget, QPushButton, QLineEdit, QGridLayout, QTableWidget, QGroupBox, QVBoxLayout,
-    QTableWidget, QLabel, QSlider
+    QTableWidget, QLabel, QSlider, QComboBox
 )
 from PyQt6.QtGui import QIcon
 from PyQt6.QtCore import Qt
+from Animated_Toggle import AnimatedToggle
 import sys
 
 
@@ -15,11 +16,12 @@ class Window(QWidget):
         super().__init__()
         # Backend
         # self.trackModel = track_model
-        # Frontend
+
+        # Window Layout
         self.setWindowIcon(QIcon("icon.jpg"))
         self.setWindowTitle("Track Model")
         self.setContentsMargins(20, 20, 20, 20)
-        self.resize(800, 600)
+        self.resize(1920//2, 1080//2)
         layout = QGridLayout()
         self.setLayout(layout)
 
@@ -48,6 +50,42 @@ class Window(QWidget):
 
         # Failure Modes
         self.failure_modes_group = QGroupBox("Failure Modes")
+
+        fm_group_layout = QGridLayout()
+
+        f1_title = QLabel()
+        f1_title.setText("Broken Rail Failure:")
+        f2_title = QLabel()
+        f2_title.setText("Track Circuit Failure:")
+        f3_title = QLabel()
+        f3_title.setText("Power Failure:")
+        fm_group_layout.addWidget(f1_title, 0, 0)
+        fm_group_layout.addWidget(f2_title, 1, 0)
+        fm_group_layout.addWidget(f3_title, 2, 0)
+
+        int_list_blocks = list(range(1, 16))
+        str_list_blocks = list(map(str, int_list_blocks))
+        combo1 = QComboBox()
+        combo1.addItems(str_list_blocks)
+        combo2 = QComboBox()
+        combo2.addItems(str_list_blocks)
+        combo3 = QComboBox()
+        combo3.addItems(str_list_blocks)
+        fm_group_layout.addWidget(combo1, 0, 1)
+        fm_group_layout.addWidget(combo2, 1, 1)
+        fm_group_layout.addWidget(combo3, 2, 1)
+
+        toggle1 = AnimatedToggle()
+        toggle1.setFixedSize(toggle1.sizeHint())
+        toggle2 = AnimatedToggle()
+        toggle2.setFixedSize(toggle2.sizeHint())
+        toggle3 = AnimatedToggle()
+        toggle3.setFixedSize(toggle3.sizeHint())
+        fm_group_layout.addWidget(toggle1, 0, 2)
+        fm_group_layout.addWidget(toggle2, 1, 2)
+        fm_group_layout.addWidget(toggle3, 2, 2)
+
+        self.failure_modes_group.setLayout(fm_group_layout)
         layout.addWidget(self.failure_modes_group, 2, 1, 1, 1)
 
         # Temperature Controls
@@ -75,9 +113,9 @@ class Window(QWidget):
         self.map_group = QGroupBox("Map")
         layout.addWidget(self.map_group, 0, 1, 2, 2)
 
-        ##############################
-        # Event Handlers
-        ##############################
+    ##############################
+    # Event Handlers
+    ##############################
     def display_slider_value(self):
         self.slider_label.setText("Environmental Temperature: " + str(self.sender().value()) + "°F")
         self.slider_label.adjustSize()  # Expands label size as numbers get larger
