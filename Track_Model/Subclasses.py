2e6d3944cc2d0f1d009f8ac745fda594df6ae6b4
DanielRichardson1 <drich1475@gmail.com>
2024-03-07 08:08:36 -0500

    3/7/2024 8AM

--- Track_Model/Subclasses.py
+++ Track_Model/Subclasses.py
@@ -1,8 +1,26 @@
 import numpy as np
 
 
+class Train:
+    def __init__(self, train_id: int):
+        self.train_id = train_id  # will match index in our trains list
+        self.block_id = 0
+
+    def set_block_id(self, block_id: int):
+        self.block_id = block_id  # We will have to implement a linked list that is updated every time a switch toggled
+
+    def get_train_id(self):
+        return self.train_id
+
+    def get_block_id(self):
+        return self.block_id
+
+class Wayside:
+    def __init__(self):
+
+
 class Station:
-    def __init__(self, block_id, s, station_side):
+    def __init__(self, block_id: int, s: str, station_side: str):
         self.block_id = block_id
         self.name = s
         self.side = station_side
@@ -41,13 +59,22 @@ class Station:
 
 
 class Signal:
-    def __init__(self, block_id):
+    def __init__(self, block_id: int):
         self.block_id = block_id
         self.value = False  # (F,T) --> (Red, Green)
 
+    def get_block_id(self):
+        return self.block_id
+
+    def get_value(self):
+        return self.value
+
+    def toggle_value(self):
+        self.value = not self.value
+
 
 class Crossing:
-    def __init__(self, block_id, s):
+    def __init__(self, block_id):
         self.block_id = block_id
         self.value = False  # (F,T) --> (Inactive, Active)
 
@@ -59,12 +86,12 @@ class Crossing:
         return self.value
 
     # setters
-    def set_value(self, x: bool):
+    def toggle_value(self, x: bool):
         self.value = x
 
 
 class Switch:
-    def __init__(self, block_id, s):
+    def __init__(self, block_id: int, s: str):
         self.block_id = block_id
         self.value = False  # (F,T) --> (Left, Right)
         self.s = s.lstrip('SWITCH (,').rstrip(')')
@@ -81,52 +108,84 @@ class Switch:
         return self.value
 
     # setters
-    def set_value(self, x: bool):
-        self.value = x
+    def toggle_value(self):
+        self.value = not self.value
 
 
 class Block:
     def __init__(self, data):
         # from data
         self.section = data[0]
-        self.id = self.section = data[1]
+        self.id = data[1]
         self.length = data[2]
         self.grade = data[3]
         self.speed_lim = data[4]
         self.elevation = data[5]
+        self.station_side = data[7]
         # add direction of travel?
 
         # defaults
-        self.occupancy = 0
+        self.occupancy = False
         self.temp = 74
         self.beacon_msg = ''
 
         # infrastructure & underground status
         self.underground = False
-        self.infrastructure = np.array([], dtype=object)
+        self.infrastructure_list = []
         if not str(data[6]) == 'nan':
-            infrastructure_list = data[6].split(';')
-            for i in range(0, len(infrastructure_list)):
-                s = infrastructure_list[i].strip()
-                if 'STATION' in s:
-                    station_side = data[7]
-                    self.infrastructure = np.append(self.infrastructure, Station(self.id, s, station_side))
-                elif s == 'RAILWAY CROSSING':
-                    self.infrastructure = np.append(self.infrastructure, Crossing(self.id, s))
-                elif 'SWITCH' in s:
-                    self.infrastructure = np.append(self.infrastructure, Switch(self.id, s))
-                    b = s.replace('-', ',').split(',')  # blocks involved in switch
-                    for j in range(len(b)):  # adding signals
-                        if b.count(b[j]) == 1:
-                            self.infrastructure = np.append(self.infrastructure, Signal(b[j]))
-                elif s == 'UNDERGROUND':
+            self.infrastructure_list = data[6].split(';')
+            for i in range(0, len(self.infrastructure_list)):
+                s = self.infrastructure_list[i].strip()
+                if s == 'UNDERGROUND':
                     self.underground = True
 
         # Beacons
         if not str(data[8]) == 'nan':
             self.beacon_msg = str(data[8])
 
+    # getters
+    def get_section(self):
+        return self.section
 
-# testing:
+    def get_id(self):
+        return self.id
 
+    def get_length(self):
+        return self.length
+
+    def get_grade(self):
+        return self.grade
+
+    def get_speed_lim(self):
+        return self.speed_lim
+
+    def get_elevation(self):
+        return self.elevation
+
+    def get_occupancy(self):
+        return self.occupancy
+
+    def get_temp(self):
+        return self.temp
+
+    def get_beacon_msg(self):
+        return self.beacon_msg
+
+    def get_underground(self):
+        return self.underground
+
+    def get_station_side(self):
+        return self.station_side
+
+    def get_infrastructure_list(self):
+        return self.infrastructure_list
+
+    # setters
+    def toggle_occupancy(self):
+        self.occupancy = not self.occupancy
+
+    def set_temp(self, x: int):
+        self.temp = x
+
+#
 
--- Track_Model/Track_Model_2.0.py
+++ Track_Model/Track_Model_2.0.py
@@ -14,15 +14,37 @@ class TrackModel:
         self.line_name = self.data[0, 0]
         self.num_blocks = len(self.data) - 1
 
+        # blocks and infrastructure objects arrays
         self.blocks = np.array([])
+        self.stations = np.array([])
+        self.signals = np.array([])
+        self.crossings = np.array([])
+        self.switches = np.array([])
         for i in range(1, self.num_blocks + 1):
-            self.blocks = np.append(self.blocks, Block(self.data[i, 1:10]))
+            block = Block(self.data[i, 1:10])
+            infrastructure_list = block.get_infrastructure_list()
+            self.blocks = np.append(self.blocks, block)
+            for j in range(0, len(infrastructure_list)):
+                s = infrastructure_list[j].strip()
+                if 'STATION' in s:
+                    station_side = block.get_station_side()
+                    self.stations = np.append(self.stations, Station(i, s, station_side))
+                elif s == 'RAILWAY CROSSING':
+                    self.crossings = np.append(self.crossings, Crossing(i))
+                elif 'SWITCH' in s:
+                    self.switches = np.append(self.switches, Switch(i, s))
+                    b = s.lstrip('SWITCH (').rstrip(')').replace('-', ',').split(',')  # blocks involved in switch
+                    for k in range(len(b)):  # adding signals
+                        if b.count(b[k]) == 1:
+                            self.signals = np.append(self.signals, Signal(b[k]))  # TODO: REFORMAT YARD SWITCHES IN XSL
+
         # defaults
         self.ticket_sales = 0
         self.environmental_temperature = 74
         self.authority = []
         self.speed = []
         self.trains = []
+        self.waysides = []
 
     def check_data(self):  # update this in future to validate data further
         if len(self.data) == 0:
@@ -39,19 +61,61 @@ class TrackModel:
     def get_ticket_sales(self):
         return self.ticket_sales
 
-    def get_environmental_temperature(self):
-        return self.environmental_temperature
-
-    def get_speed(self):
-        return self.speed
-
-    def get_authority(self):
-        return self.authority
+    def get_section(self, section_id_start, section_id_end=None):
+        pass
 
     def get_blocks(self):
         return self.blocks
 
+    def set_data(self, file_name: str):
+        pass  # object completely reinitialized
+
+    def set_block_occupancy(self, block_id: int, occupancy: bool):
+        self.blocks[block_id-1] = occupancy
+
+    def get_environmental_temperature(self):
+        return self.environmental_temperature
+
+    def get_stations(self):
+        return self.stations
+
+    def get_switches(self):
+        return self.switches
+
+    def get_crossings(self):
+        return self.crossings
+
+    def get_signals(self):
+        return self.signals
+
     # communication w/ other modules
+
+    # setters
+    def update_authority(self, authority: list[int]):
+        self.authority = authority
+
+    def update_speed(self, speed: list[float]):
+        self.speed = speed
+
+    def toggle_switch(self, block_id: int):
+        pass
+
+    def toggle_signal(self, block_id: int):
+        pass
+
+    def toggle_crossing(self, block_id: int):
+        pass
+
+    def train_spawned(self):
+        pass
+
+    def train_presence_changed(self, train_id):
+        pass
+
+    def set_disembarking_passengers(self, train_id, disembarking_passengers):
+        pass
+
+    # getters
     def get_tc_block_occupancy(self, starting_block: int, ending_block: int):
         result = []
         for i in range(starting_block, ending_block + 1):
@@ -76,15 +140,12 @@ class TrackModel:
     def get_tm_underground_status(self, train_id: int) -> bool:
         return self.blocks[self.trains[train_id].get_block_id()].get_underground()
 
-    def get_tm_embarking_passengers(self, train_id: int) -> int:
-        block_infrastructure = self.blocks[self.trains[train_id].get_block_id()].get_infrastructure()
+    def get_tm_embarking_passengers(self, train_id: int) -> int:  # relies on stations being first entry in table
+        return self.blocks[self.trains[train_id].get_block_id()].get_infrastructure()[0].get_embarking()
 
-    # ctc
     def get_ctc_ticket_sales(self):
         pass
 
 
 # temp main
 t = TrackModel('Green Line.xlsx')
-print(t.get_blocks[])
-
