4473c2b898f371104c11e9b3c1eebc22f134b9da
Noah Lichstein <noahlichstein@gmail.com>
2024-03-24 18:44:54 -0400

    SystemTime Testing in Progress

--- SystemTime/SystemTime.py
+++ SystemTime/SystemTime.py
@@ -1,31 +1,45 @@
 from threading import Timer
-import threading
+from multiprocessing import Process, Value
 import time as python_time
 
+def update_time(timer_flag, update_interval, sys_time):
+    sys_time_float = float(sys_time.value)
+
+    while(timer_flag.value):
+        # python_time.sleep(update_interval.value)
+
+        start_time = python_time.time()
+
+        while(True):
+            if(python_time.time() >= start_time + update_interval.value):
+                break
+        sys_time_float = sys_time_float + 0.1
+        # print("time updated %f", sys_time_float)
+        sys_time.value = sys_time_float # 0.1 s precison
 
 class SystemTime:
     def __init__(self):
-        self.__sys_time:float = python_time.time()
         self.__scale:float = 1
-        self.__update_interval:float = 0.1
 
-        self.__timer_flag = True
-        self.__time_update_thread = threading.Thread(target=self.__update_time)
-        self.__time_update_thread.start()
+        init_time = python_time.time()
+        # print(init_time)
+        self.__sys_time = Value('d', init_time)
+        self.__update_interval = Value('d', 0.1)
+        self.__timer_flag = Value('i', 1)
+
+        self.__time_update_process= Process(target=update_time, args=(self.__timer_flag, self.__update_interval, self.__sys_time, ))
+        self.__time_update_process.start()
 
     def __del__(self):
-        self.__timer_flag = False
-        self.__time_update_thread.join()
+        self.__timer_flag.value = 0
+        self.__time_update_process.join()
     
     def time(self)->float:
-        return self.__sys_time
+        # print("On ret: %f", self.__sys_time.value)
+        sys_time = self.__sys_time.value
+        return sys_time
     
     def set_multiplier(self, multiplier:float)->None:
         self.__scale = multiplier
-        self.__update_interval = 1.0 / (10 * self.__scale)
+        self.__update_interval.value = 1.0 / (10 * self.__scale)
 
-    def __update_time(self):
-
-        while(self.__timer_flag):
-            python_time.sleep(self.__update_interval)
-            self.__sys_time += 0.1 # 1 s precison
--- SystemTime/Tests/test_SystemTime.py
+++ SystemTime/Tests/test_SystemTime.py
@@ -3,11 +3,10 @@ import unittest
 from SystemTime import SystemTime
 
 class TestSystemTime(unittest.TestCase):
-    def test_1s_multiplier(self):
+    def test_1s_multiplier1(self):
         system_time = SystemTime()
         timer_0 = time.time()
         t_0 = system_time.time()
-
 
         time.sleep(1)
 
@@ -15,4 +14,75 @@ class TestSystemTime(unittest.TestCase):
 
         time_elapsed = t_1 - t_0
 
-        self.assertAlmostEqual(time_elapsed, 1.0, 2, "time difference does not equal 1s")
+        self.assertAlmostEqual(time_elapsed, 1.0, 0, "time difference does not equal 1s")
+
+    def test_1s_multiplier2(self):
+        system_time = SystemTime()
+        timer_0 = time.time()
+        system_time.set_multiplier(2)
+        t_0 = system_time.time()
+
+        time.sleep(1)
+
+        t_1 = system_time.time()
+
+        time_elapsed = t_1 - t_0
+
+        self.assertAlmostEqual(time_elapsed, 2.0, 0, "time difference does not equal 2s")
+
+    def test_10s_multiplier1(self):
+        system_time = SystemTime()
+        timer_0 = time.time()
+        system_time.set_multiplier(1)
+        t_0 = system_time.time()
+
+        time.sleep(10)
+
+        t_1 = system_time.time()
+
+        time_elapsed = t_1 - t_0
+
+        self.assertAlmostEqual(time_elapsed, 10.0, 0, "time difference does not equal 10s")
+
+    def test_10s_multiplier2(self):
+        system_time = SystemTime()
+        timer_0 = time.time()
+        system_time.set_multiplier(2)
+        t_0 = system_time.time()
+
+        time.sleep(10)
+
+        t_1 = system_time.time()
+
+        time_elapsed = t_1 - t_0
+
+        self.assertAlmostEqual(time_elapsed, 20.0, 0, "time difference does not equal 20s")
+
+    def test_60s_multiplier10(self):
+        system_time = SystemTime()
+        timer_0 = time.time()
+        system_time.set_multiplier(10)
+        t_0 = system_time.time()
+
+        time.sleep(60)
+
+        t_1 = system_time.time()
+
+        time_elapsed = t_1 - t_0
+
+        self.assertAlmostEqual(time_elapsed, 600, 0, "time difference does not equal 600s")
+
+
+    def test_172800s_multiplier10(self):
+        system_time = SystemTime()
+        timer_0 = time.time()
+        system_time.set_multiplier(10)
+        t_0 = system_time.time()
+
+        time.sleep(600000)
+
+        t_1 = system_time.time()
+
+        time_elapsed = t_1 - t_0
+
+        self.assertAlmostEqual(time_elapsed, 17280, 0, "time difference does not equal 172800s")
