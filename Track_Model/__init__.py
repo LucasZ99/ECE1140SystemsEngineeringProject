6d3edf647491eff1eac96734c6139a884e1007c9
Lucas Connell <lzc6@pitt.edu>
2024-03-31 16:49:33 -0400

    integration maybe?

--- Track_Model/__init__.py
+++ Track_Model/__init__.py
@@ -1,4 +1,5 @@
 from .Track_Model_UI import Window
 from .Track_Model_TB_UI import TestBenchWindow
 from .Track_Model import TrackModel
+from .animated_toggle import AnimatedToggle
 # from .paths import AdjacencyList
