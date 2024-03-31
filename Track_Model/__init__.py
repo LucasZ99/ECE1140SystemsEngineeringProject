951320388b35b9553077bfee969251ea5ef2279e
DanielRichardson1 <drich1475@gmail.com>
2024-03-31 16:49:10 -0400

    3/31/2024 5 pm

--- Track_Model/__init__.py
+++ Track_Model/__init__.py
@@ -2,3 +2,4 @@ from .Track_Model_UI import Window
 from .Track_Model_TB_UI import TestBenchWindow
 from .Track_Model import TrackModel
 # from .paths import AdjacencyList
+from .Animated_Toggle import AnimatedToggle
