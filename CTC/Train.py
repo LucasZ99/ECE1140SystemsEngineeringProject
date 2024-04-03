ade07d59cd18177d1bf43ac84f0e434f016ffc85
Noah Lichstein <noahlichstein@gmail.com>
2024-04-02 22:13:52 -0400

    routing updates

--- CTC/Train.py
+++ CTC/Train.py
@@ -1,4 +1,4 @@
-from CTC.Route import Stop
+from CTC.Route import Stop, Route
 from SystemTime import SystemTime
 from CTC.Track import MSSD, TRACK, GREEN_LINE_YARD_SPAWN, GREEN_LINE_YARD_DELETE
 
@@ -12,18 +12,39 @@ class Train():
         self.current_stop = 0
         self.current_block = GREEN_LINE_YARD_SPAWN
         self.authority = 0
+        self.at_last_stop = False
+
+        # determine slowdown of route
+        self.speed_factor = Route().get_route_travel_time(
+            Route().get_times_through_route(self.line_id, [stop.block for stop in self.route])
+        ) / Route().get_route_travel_time(self.route)
 
         self.system_time = system_time
+
+    def get_slowdown(self) -> float:
+        return self.speed_factor
 
     def get_next_block(self) -> int:
         return self.next_block(self.current_block)
 
-    def departure_time(self):
+    def departure_time(self) -> float:
         return self.route[self.current_stop].departure_time
 
-    def set_to_next_block(self):
+    def arrival_time(self) -> float:
+        return self.route[self.next_stop].arrival_time
+
+    def blocks_to_next_stop(self) -> int:
+        return len(Route().dfs_find_route(self.line_id, self.current_block, self.route[self.next_stop]))
+
+    """
+    Sets the train to the next block.
+    :returns 0 for continuing to next station, 1 for at next station, -1 for last station - dispatch to yard, or -2 for reached yard.
+    """
+
+    def set_to_next_block(self) -> int:
         self.current_block = self.get_next_block()
 
+        # check if at next stop
         if self.route[self.next_stop].block == self.current_block:
             # check if route is delayed
             if self.system_time.time() > self.route[self.next_stop].arrival_time:
@@ -36,8 +57,20 @@ class Train():
             self.current_stop += 1
             self.next_stop += 1
 
-            if self.next_stop >= self.route.__len__():
+            # at yard
+            if self.current_stop == self.route.__len__() - 1:
                 self.at_last_stop = True
+                return -2
+            # last stop - yard next
+            elif self.current_stop == self.route.__len__() - 2:
+                return -1
+            # otherwise, intermediate stop
+            else:
+                return 1
+
+        # not at stop
+        else:
+            return 0
 
     def next_block(self, block: int) -> int:
         next_blocks = TRACK[self.line_id][block]
@@ -57,7 +90,8 @@ class Train():
         return self.next_stop
 
     def get_destination(self) -> Stop:
-        return self.route[-1]
+        # route[-1] is the yahd.
+        return self.route[-2]
 
     def add_stop(self, stop: Stop):
         self.route.append(stop)
@@ -74,16 +108,15 @@ class Train():
             self.at_last_stop = False
             self.next_stop += 1
 
-    # returns the next MSSD blocks OR blocks until next stop (including current block)
+    # returns the next MSSD blocks OR blocks until next stop (excluding current block)
     def get_next_blocks(self) -> list[int]:
 
-        next_blocks = [self.current_block]
+        next_blocks = []
 
         prev_block = self.current_block
 
         for i in range(1, MSSD + 1):
             next_block = self.next_block(prev_block)
-
             if next_block == self.route[self.next_stop].block:
                 next_blocks.append(next_block)
 
