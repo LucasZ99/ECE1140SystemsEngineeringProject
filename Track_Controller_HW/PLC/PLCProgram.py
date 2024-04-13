604394677fa12c5f6479b1f24ca4b66ca2c1aad4
Devin James <45702781+DJames122@users.noreply.github.com>
2024-04-13 19:48:07 -0400

    Padding fixed v2
    should be good to connect to main wayside container class now

--- Track_Controller_HW/PLC/PLCProgram.py
+++ Track_Controller_HW/PLC/PLCProgram.py
@@ -39,25 +39,25 @@ class PLC:  # watchdog behavior for the PLC data
             elif self.blocks.get(i, False):
                 self.stops[i] = True
                 if i > 29:
-                    self.stops.setdefault(i - 1, True)
+                    self.stops[i - 1] = True
                 if i > 30:
-                    self.stops.setdefault(i - 2, True)
+                    self.stops[i - 2] = True
                 if i > 31:
-                    self.stops.setdefault(i - 3, True)
+                    self.stops[i - 3] = True
                 if i > 32:
-                    self.stops.setdefault(i - 4, True)
+                    self.stops[i - 4] = True
 
         for i in range(101, 150):  # blocks 101 - 150
             if self.blocks.get(i, False):
                 self.stops[i] = True
                 if i > 101:
-                    self.stops.setdefault(i - 1, True)
+                    self.stops[i - 1] = True
                 if i > 102:
-                    self.stops.setdefault(i - 2, True)
+                    self.stops[i - 2] = True
                 if i > 103:
-                    self.stops.setdefault(i - 3, True)
+                    self.stops[i - 3] = True
                 if i > 104:
-                    self.stops.setdefault(i - 4, True)
+                    self.stops[i - 4] = True
 
         #print("stops:", self.stops)
 
