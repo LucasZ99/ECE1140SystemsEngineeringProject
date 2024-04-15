7ab9d032bf986c3fb1cd643373021b739e40603f
Lucas Connell <lzc6@pitt.edu>
2024-04-15 17:23:46 -0400

    fixed wayside PLC execution error

--- Track_Controller_SW/PLC_Logic.py
+++ Track_Controller_SW/PLC_Logic.py
@@ -1,6 +1,8 @@
 import os
 import subprocess
 import json
+import pickle
+
 
 class PlcProgram(object):
     def __init__(self, section: str):
@@ -11,26 +13,29 @@ class PlcProgram(object):
         elif section == "C":
             self.filepath = os.path.join(current_dir, 'PLCProgramSectionC.py')
 
-
     def set_filepath(self, filepath: str):
         self.filepath = filepath
         print(f"filepath: {self.filepath}")
 
     def execute_plc(self, occupancy_list: list[bool]):
 
-        arg_json = json.dumps(occupancy_list)
+        with open('occupancy_file.pkl', 'wb') as f:
+            pickle.dump(occupancy_list, f)
+
         try:
-            result = subprocess.check_output(['python', self.filepath, arg_json], stderr=subprocess.STDOUT)
-            result_decoded = result.decode('utf-8')
-            result_list = json.loads(result_decoded)
-            print("result list: ", result_list)
-            return result_list
-        except subprocess.CalledProcessError as e:
-            # Handle any errors
-            print("Error:", e.output.decode('utf-8'))
+            subprocess.check_output(['python', self.filepath],
+                                         stderr=subprocess.STDOUT)
+        except Exception as e:
+            print(e)
+            raise e
+
+        with open('plc_result.pkl', 'rb') as f:
+            result = pickle.load(f)
+
+        print("result list: ", result)
+        return result
 
 
-# if __name__ == "__main__":
-#     program = PlcProgram()
-#     output = program.execute_plc([True, False, False], section="A")
-#     print(output)
+if __name__ == "__main__":
+    program = PlcProgram("A")
+    output = program.execute_plc([False] * 32)
