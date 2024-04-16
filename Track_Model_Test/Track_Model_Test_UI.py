82b502be17cc49bc81b56d4f548b606bb468512b
DanielRichardson1 <drich1475@gmail.com>
2024-04-16 19:55:03 -0400

    Working testbench

--- Track_Model_Test/Track_Model_Test_UI.py
+++ Track_Model_Test/Track_Model_Test_UI.py
@@ -51,7 +51,7 @@ class TrackModelTestUI(QMainWindow):
         self.switch_changed_indexes_input.editingFinished.connect(self.switch_changed_indexes_input_handler)
         self.signal_changed_indexes_label = QLabel('signal_changed_indexes:')
         self.signal_changed_indexes_input = QLineEdit()
-        self.signal_changed_indexes_input.setText('[]')
+        self.signal_changed_indexes_input.setText('[(1, False), (12, False), (29, False), (76. False)]')
         self.signal_changed_indexes_input.editingFinished.connect(self.signal_changed_indexes_input_handler)
         self.rr_crossing_indexes_label = QLabel('rr_crossing_indexes:')
         self.rr_crossing_indexes_input = QLineEdit()
