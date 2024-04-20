72d4e29dbc771da8a9f0a6daaa591a016411a02d
Lucas Connell <lzc6@pitt.edu>
2024-04-20 14:29:52 -0400

    ctc prepped

--- mini_launcher_ui.py
+++ mini_launcher_ui.py
@@ -13,22 +13,32 @@ from Track_Controller_SW.TrackControllerUI import UI
 
 
 class Ui_MainWindow(QMainWindow):
-    def __init__(self, track_controller_test_signals: TrackControllerTestSignals, track_controller_signals: TrackControllerSignals):
+    def __init__(self,
+                 # ctc_signals = CTCSignals,
+                 track_controller_test_signals: TrackControllerTestSignals,
+                 track_controller_signals: TrackControllerSignals):
         super().__init__()
         loadUi('mini_launcher.ui', self)
         self.test_ui = TestUi(track_controller_test_signals)
         self.track_controller_a_ui = UI("A", track_controller_signals)
         self.track_controller_c_ui = UI("C", track_controller_signals)
+        # self.ctc_ui = CTCUi(ctc_signals)
 
         self.track_controller_test_button = self.findChild(QPushButton, "track_controller_test_button")
         self.track_controller_a_button = self.findChild(QPushButton, "track_controller_a_button")
         self.track_controller_c_button = self.findChild(QPushButton, "track_controller_c_button")
+        self.ctc_button = self.findChild(QPushButton, "ctc_button")
 
         self.track_controller_test_button.clicked.connect(self.open_test_ui)
         self.track_controller_a_button.clicked.connect(self.open_track_controller_a_ui)
         self.track_controller_c_button.clicked.connect(self.open_track_controller_c_ui)
+        self.ctc_button.clicked.connect(self.open_ctc_ui)
 
         self.show()
+
+    def open_ctc_ui(self):
+        # self.ctc_ui.show()
+        pass
 
     def open_test_ui(self):
         self.test_ui.show()
@@ -47,23 +57,31 @@ def main():
     top_level_signals = TopLevelSignals()
     track_controller_signals = TrackControllerSignals()
     track_controller_test_signals = TrackControllerTestSignals()
+    # ctc_signals = CTCSignals()
+
+    # ctc_thread = QThread()
+    # ctc_container = CTCContainer(top_level_signals=top_level_signals,
+    #                              signals=ctc_signals)
+    # ctc_container.moveToThread(ctc_thread)
 
     test_thread = QThread()
     test_container = TrackControllerTestBenchContainer(top_level_signals=top_level_signals,
                                                        signals=track_controller_test_signals)
     test_container.moveToThread(test_thread)
 
-
     track_controller_thread = QThread()
     track_controller_container = TrackControllerContainer(top_level_signals=top_level_signals,
                                                           signals=track_controller_signals)
     track_controller_container.moveToThread(track_controller_thread)
 
+    # ctc_thread.start()
     test_thread.start()
     track_controller_thread.start()
 
-    window = Ui_MainWindow(track_controller_test_signals=track_controller_test_signals,
-                           track_controller_signals=track_controller_signals)
+    window = Ui_MainWindow(
+        # ctc_signals=ctc_signals,
+        track_controller_test_signals=track_controller_test_signals,
+        track_controller_signals=track_controller_signals)
 
     window.show()
     sys.exit(app.exec())
